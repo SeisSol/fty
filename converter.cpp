93e95df27aa870c651459e35b4b801668bf32c6a
ravil <ravil.dorozhinskii@tum.de>
2020-08-17 21:31:51 +0200

    Changed policy in the example

--- converter.cpp
+++ converter.cpp
@@ -10,8 +10,7 @@ int main(int Argc, char *Argv[]) {
   }
 
   std::string FileName = Argv[1];
-  //fty::Loader<fty::As_lowercase> Loader{};
-  fty::Loader<fty::As_original> Loader{};
+  fty::Loader<fty::As_lowercase> Loader{};
   try {
     YAML::Node Params = Loader.load(FileName);
     std::cout << Params;
